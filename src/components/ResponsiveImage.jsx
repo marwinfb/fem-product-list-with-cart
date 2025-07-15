f9f1831f96a28352e50f538ca507ee93448f1339
Marwin <marwinfb@gmail.com>
2025-07-15 14:59:45 +0800

    test image path

--- src/components/ResponsiveImage.jsx
+++ src/components/ResponsiveImage.jsx
@@ -1,9 +1,11 @@
+import img from "../assets/images/image-baklava-desktop.jpg"
+
 function ResponsiveImage({ mobile, tablet, desktop, alt }) {
   return (
     <picture>
-      <source media="(min-width: 1024px)" srcSet={desktop} />
-      <source media="(min-width: 768px)" srcSet={tablet} />
-      <img src={mobile} alt={alt} className="w-full h-auto hover:scale-110 transition" loading="lazy"/>
+      {/* <source media="(min-width: 1024px)" srcSet={desktop} />
+      <source media="(min-width: 768px)" srcSet={tablet} /> */}
+      <img src={img} alt={alt} className="w-full h-auto hover:scale-110 transition" loading="lazy"/>
     </picture>
   )
 }
