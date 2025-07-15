83eac9b010f1e9a895dc77c0014ada7323a76e89
Marwin <marwinfb@gmail.com>
2025-07-15 14:53:49 +0800

    fix image path of json file

--- src/components/ResponsiveImage.jsx
+++ src/components/ResponsiveImage.jsx
@@ -1,11 +1,10 @@
 function ResponsiveImage({ mobile, tablet, desktop, alt }) {
   return (
-    <img src={desktop} alt={alt} className="w-full h-auto hover:scale-110 transition" loading="lazy"/>
-    // <picture>
-    //   <source media="(min-width: 1024px)" srcSet={desktop} />
-    //   <source media="(min-width: 768px)" srcSet={tablet} />
-    //   <img src={mobile} alt={alt} className="w-full h-auto hover:scale-110 transition" loading="lazy"/>
-    // </picture>
+    <picture>
+      <source media="(min-width: 1024px)" srcSet={desktop} />
+      <source media="(min-width: 768px)" srcSet={tablet} />
+      <img src={mobile} alt={alt} className="w-full h-auto hover:scale-110 transition" loading="lazy"/>
+    </picture>
   )
 }
 export default ResponsiveImage
